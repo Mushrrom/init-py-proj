c9db9982da554bfec98867668707a11ac4ddbd57
Mushrrom <94596520+Mushrrom@users.noreply.github.com>
2023-07-31 21:22:32 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import inspect
+import os.path
+import os
+import sys
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
+
+current_directory = os.getcwd()
+if len(sys.argv) == 2:
+    print(f"creating project with name: {sys.argv[1]}")
+    project_name = sys.argv[1]
+else:
+    project_name = input("Enter project name: ")
+
+project_path = current_directory + "/" + project_name
+
+if os.path.exists(project_path):
+    print("project already exists")
+    sys.exit(1)
+
+os.mkdir(project_path)
+print(project_path)
+with open(f"{path}/.pylintrc", "r") as f:
+    pylintsettings = f.read()
+
+with open(f"{project_path}/.pylintrc", "w") as f:
+    f.write(pylintsettings)
+
+with open(f"{path}/pyrightconfig.json", "r") as f:
+    pyrightconfig = f.read()
+
+with open(f"{project_path}/pyrightconfig.json", "w") as f:
+    f.write(pyrightconfig)
+
+with open(f"{project_path}/.gitignore", "w") as f:
+    f.write(".pylintrc\n.pyrightconfig.json")
+
+with open(f"{project_path}/requirements.txt", "w") as f:
+    f.write("")
+
+with open(f"{project_path}/main.py", "w") as f:
+    f.write("")
+
+print("created project :)")
---
+++ readme.md
@@ -0,0 +1,4 @@
+# project creator
+A simple python script I made to automatically initialise a python project for me. It automatically sets up a .gitignore, my pyright and pylint configs, and requirements.txt, and main.py for python projects.
+
+To use it simply add this directory to PATH on windows and run init-py-proj to create a new project.
